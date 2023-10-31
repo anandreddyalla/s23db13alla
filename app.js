fd4c90d615325f839cdfa6c2801249940dcac6e1
anandreddyalla <143015686+anandreddyalla@users.noreply.github.com>
2023-10-31 11:20:42 -0500

    all the required functionalities has been added

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const carsRouter = require('./routes/cars'); 
+const boardRouter = require('./routes/board');
+
+
+
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -22,18 +26,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
+app.use('/cars', carsRouter);
+
+app.use('/board', boardRouter);
+
+app.get('/choose', function (req, res) {
+  res.render('choose', { title: 'Choose' });
+});
+
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/board.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET board page. */
+router.get('/', function(req, res, next) {
+  const query = req.query;
+  console.log(`rows ${query.rows}`);
+  console.log(`cols ${query.cols}`);
+  res.render('board', { title: 'Board Display', query });
+});
+
+module.exports = router;
