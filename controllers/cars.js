6fcaf0dc2b22eb2a9d94e75a8626ad6744c6e983
anandreddyalla <143015686+anandreddyalla@users.noreply.github.com>
2023-11-10 01:23:51 -0600

    changes made for Id and put methods

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,62 +1,95 @@
 var Car = require('../models/cars');
-exports.cars_list = function(req, res) {
- res.send('NOT IMPLEMENTED: Cars list');
+exports.cars_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: Cars list');
 };
 
-exports.cars_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Cars detail: ' + req.params.id);
+exports.cars_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Cars detail: ' + req.params.id);
 };
 
-exports.cars_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Cars create POST');
+exports.cars_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Cars create POST');
 };
 
-exports.cars_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Cars delete DELETE ' + req.params.id);
+exports.cars_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Cars delete DELETE ' + req.params.id);
 };
 
-exports.cars_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Cars update PUT' + req.params.id);
+exports.cars_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Cars update PUT' + req.params.id);
 };
 
 
-exports.cars_list = async function(req, res) {
-    try{
-    theCars = await Car.find();
-    res.send(theCars);
+exports.cars_list = async function (req, res) {
+    try {
+        theCars = await Car.find();
+        res.send(theCars);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
-
-
-exports.cars_view_all_Page = async function(req, res) {
-    try{
-    theCars = await Car.find();
-    res.render('cars', { title: 'Cars Search Results', results: theCars });
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
+};
 
-   
-exports.cars_create_post = async function(req, res) {
+
+exports.cars_view_all_Page = async function (req, res) {
+    try {
+        theCars = await Car.find();
+        res.render('cars', { title: 'Cars Search Results', results: theCars });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+
+exports.cars_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Car();
-    
+
     document.cars_make = req.body.cars_make;
     document.cars_model = req.body.cars_model;
     document.cars_year = req.body.cars_year;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+
+exports.cars_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Car.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+
+exports.cars_update_put = async function (req, res) {
+    console.log(`Update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+
+    try {
+        let toUpdate = await Car.findById(req.params.id);
+
+        // Update properties if they are defined in the request body
+        if (req.body.cars_make) toUpdate.cars_make = req.body.cars_make;
+        if (req.body.cars_model) toUpdate.cars_model = req.body.cars_model;
+        if (req.body.cars_year) toUpdate.cars_year = req.body.cars_year;
+        if (req.body.checkboxsale) toUpdate.sale = true; // Convert checkbox value to boolean
+
+        let result = await toUpdate.save();
+        console.log("Success " + result);
+        res.send(result);
+    } catch (err) {
+        res.status(500).send(`{"error": "${err}": Update for id ${req.params.id} failed`);
+    }
+};
--- routes/cars.js
+++ routes/cars.js
@@ -3,5 +3,9 @@ const cars_controllers= require('../controllers/cars');
 var router = express.Router();
 
 router.get('/', cars_controllers.cars_view_all_Page );
+
+router.get('/cars/:id', cars_controllers.cars_detail);
+
+
 module.exports = router;
 
