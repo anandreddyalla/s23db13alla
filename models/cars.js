57dff52e283e2a94ebf1e3cd19c795d652fa4408
anandreddyalla <143015686+anandreddyalla@users.noreply.github.com>
2023-11-29 21:30:27 -0600

    changes made in router and models files

--- models/cars.js
+++ models/cars.js
@@ -2,9 +2,24 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
-  cars_make: String,
-  cars_model: String,
-  cars_year: Number,
+  cars_make: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 10
+},
+  cars_model: {
+    type: String,
+    required: true,
+    minlength: 0,
+    maxlength: 7
+},
+  cars_year: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10000
+    }
 });
 
 const Car = mongoose.model('Car', carSchema);
--- routes/cars.js
+++ routes/cars.js
@@ -1,20 +1,34 @@
 var express = require('express');
-const cars_controllers= require('../controllers/cars');
+const passport = require('passport');
+const cars_controllers = require('../controllers/cars');
 var router = express.Router();
 
-router.get('/', cars_controllers.cars_view_all_Page );
+router.get('/', cars_controllers.cars_view_all_Page);
 
 router.get('/cars/:id', cars_controllers.cars_detail);
 
-router.get('/detail', cars_controllers.cars_view_one_Page);
-
-router.get('/create', cars_controllers.cars_create_Page);
-
-router.get('/update', cars_controllers.cars_update_Page);
-
-router.get('/delete', cars_controllers.cars_delete_Page);
 
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+// router.get('/update', secured, cars_controllers.cars_update_Page);
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
+
+router.get('/detail', secured, cars_controllers.cars_view_one_Page);
+
+router.get('/create', secured, cars_controllers.cars_create_Page);
+
+router.get('/update', secured, cars_controllers.cars_update_Page);
+
+router.get('/delete', secured, cars_controllers.cars_delete_Page);
 
 module.exports = router;
 
